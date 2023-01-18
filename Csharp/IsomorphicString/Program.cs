114315d10fd15ef0c4adc9c9a0347b4a76b9bc52
Mustafa Sahin <Sahin-Mustafa@gmail.com>
2023-01-19 01:27:39 +0300

    closed

--- Csharp/IsomorphicString/Program.cs
+++ Csharp/IsomorphicString/Program.cs
@@ -12,18 +12,18 @@ namespace IsomorphicString
 
         private static void TestCase()
         {
-            //bool result = IsIsomorphic("egg", "add");
-            //Console.WriteLine($"Test 1: {result}\n----");
+            bool result = IsIsomorphic("egg", "add");
+            Console.WriteLine($"Test 1: {result}\n----");
 
 
-            //bool result2 = IsIsomorphic("foo", "bar");
-            //Console.WriteLine($"Test 2: {result2}\n----");
+            bool result2 = IsIsomorphic("foo", "bar");
+            Console.WriteLine($"Test 2: {result2}\n----");
 
-            //bool result3 = IsIsomorphic("paper", "title");
-            //Console.WriteLine($"Test 3: {result3}\n----");
+            bool result3 = IsIsomorphic("paper", "title");
+            Console.WriteLine($"Test 3: {result3}\n----");
 
-            //bool result4 = IsIsomorphic("badc", "baba");
-            //Console.WriteLine($"Test 4: {result4}\n----");
+            bool result4 = IsIsomorphic("badc", "baba");
+            Console.WriteLine($"Test 4: {result4}\n----");
 
             bool result5 = IsIsomorphic("abcdefghijklmnopqrstuvwxyzva", "abcdefghijklmnopqrstuvwxyzck");
             Console.WriteLine($"Test 5: {result5}\n----");
@@ -43,7 +43,7 @@ namespace IsomorphicString
             {
                 if (!match.ContainsKey(s[i].ToString()))
                 {
-                    match.Add(new KeyValuePair<string, int>(s[i].ToString(), i));
+                    match.Add(s[i].ToString(), i);
                 }
                 sb.Append($"{match[s[i].ToString()]}-");
                 
